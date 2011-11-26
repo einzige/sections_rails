f2887ccf6d9c77bf06bee0ac5862149a01578010
Kevin Goslar <kevin.goslar@gmail.com>
2011-11-25 22:29:56 -0800

    main helper method

--- lib/sections_rails.rb
+++ lib/sections_rails.rb
@@ -1,2 +1,17 @@
 module SectionsRails
+  
+  def section name
+    out = []
+    out << javascript_include_tag("#{name}/#{name}") if File.exists? "#{Rails.root}/app/sections/#{name}/#{name}.js"
+    out << stylesheet_link_tag("#{name}/#{name}") if File.exists? "#{Rails.root}/app/sections/#{name}/#{name}.css"
+    if File.exists? "#{Rails.root}/app/sections/#{name}/_#{name}.html.erb" 
+      out << render(:partial => "/../sections/#{name}/#{name}")
+    else
+      out << "<div class=\"#{name}\"></div>"
+    end
+    out.join("\n").html_safe
+  end
+  
 end
+
+ActionView::Base.send :include, SectionsRails
