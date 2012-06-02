821d7defcd2a869eef0d121f2fc74f667d1c718c
Kevin Goslar <kevin.goslar@originate.com>
2012-06-02 07:45:02 -0700

    Configuration options for JS and CSS.

--- lib/sections_rails.rb
+++ lib/sections_rails.rb
@@ -2,7 +2,7 @@ module SectionsRails
 
   require "sections_rails/railtie" if defined?(Rails)
   
-  def section combined_name
+  def section combined_name, options = {}
     out = []
 
     # Split the parameter into file name and directory name.
@@ -15,8 +15,28 @@ module SectionsRails
     # Add assets of section when in dev mode.
     file_path = "#{directory_path}/#{filename}"                              # Base path of filename in section: /app/sections/admin/logo/logo
     if Rails.env != 'production'
-      out << javascript_include_tag("#{directory}#{filename}/#{filename}") if File.exists?("#{file_path}.js") || File.exists?("#{file_path}.js.coffee") || File.exists?("#{file_path}.coffee")
-      out << stylesheet_link_tag("#{directory}#{filename}/#{filename}") if File.exists?("#{file_path}.css") || File.exists?("#{file_path}.css.scss") || File.exists?("#{file_path}.css.sass") || File.exists?("#{file_path}.scss") || File.exists?("#{file_path}.sass") 
+
+      # Include JS assets.
+      if options.has_key? :js
+        if options[:js]
+          out << javascript_include_tag("#{directory}#{filename}/#{options[:js]}")
+        end
+      else
+        if File.exists?("#{file_path}.js") || File.exists?("#{file_path}.js.coffee") || File.exists?("#{file_path}.coffee")
+          out << javascript_include_tag("#{directory}#{filename}/#{filename}")
+        end
+      end
+
+      # Include CSS assets.
+      if options.has_key? :css
+        if options[:css]
+          out << stylesheet_link_tag("#{directory}#{filename}/#{options[:css]}")
+        end
+      else
+        if File.exists?("#{file_path}.css") || File.exists?("#{file_path}.css.scss") || File.exists?("#{file_path}.css.sass") || File.exists?("#{file_path}.scss") || File.exists?("#{file_path}.sass") 
+          out << stylesheet_link_tag("#{directory}#{filename}/#{filename}")
+        end
+      end
     end
 
     # Render the section partial into the view.
--- lib/sections_rails/version.rb
+++ lib/sections_rails/version.rb
@@ -1,3 +1,3 @@
 module SectionsRails
-  VERSION = "0.3.0"
+  VERSION = "0.4.0"
 end
